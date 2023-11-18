8011680605e88d3f8656d32aaa1b4f5ac1f66d46
jack <jackg2667@protonmail.com>
2023-11-18 14:30:50 +0000

    adding some security

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVT5hgX8hx2yiVW9fUOu6GLH6E21y-gtE",
-  authDomain: "auth-v2-ab357.firebaseapp.com",
-  projectId: "auth-v2-ab357",
-  storageBucket: "auth-v2-ab357.appspot.com",
-  messagingSenderId: "820424131738",
-  appId: "1:820424131738:web:678a27d1208449e5a28b23",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
