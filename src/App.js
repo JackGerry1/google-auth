03e99726e88c150398f3a013b8a8140a0cf349ac
jack <jackg2667@protonmail.com>
2023-11-18 13:46:53 +0000

    created google sign in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Protected from "./components/Protected";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Something here</h1>
-      <h1>Something here</h1>
-      <h1>Something here</h1>
-      <h1>Something here</h1>
+    <div>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route
+            path="/account"
+            element={
+              <Protected>
+                <Account />
+              </Protected>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
